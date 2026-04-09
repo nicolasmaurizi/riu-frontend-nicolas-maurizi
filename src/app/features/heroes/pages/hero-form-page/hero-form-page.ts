a99214fc5875e0ccba285e76a981feda9138695f
Your Namenicolasmaurizi <nicolasmaurizi@gmail.com>
2026-04-09 18:06:18 -0300

    feat: scaffold hero form page

--- src/app/features/heroes/pages/hero-form-page/hero-form-page.ts
+++ src/app/features/heroes/pages/hero-form-page/hero-form-page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero-form-page',
+  standalone: true,
   imports: [],
   templateUrl: './hero-form-page.html',
   styleUrl: './hero-form-page.scss',
