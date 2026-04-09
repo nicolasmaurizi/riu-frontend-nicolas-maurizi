34c0e63d486cb2e38d363c24ca0a6d0db203d39b
Your Namenicolasmaurizi <nicolasmaurizi@gmail.com>
2026-04-09 19:25:39 -0300

    feat: add heroes page container

--- src/app/features/heroes/pages/heroes-page/heroes-page.ts
+++ src/app/features/heroes/pages/heroes-page/heroes-page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HeroesHome } from '../../components/heroes-home/heroes-home';
+import { LayoutShell } from '../../../../shared/components/layout-shell/layout-shell';
+
 @Component({
   selector: 'app-heroes-page',
   standalone: true,
-  imports: [],
+  imports: [LayoutShell, HeroesHome],
   templateUrl: './heroes-page.html',
   styleUrl: './heroes-page.scss',
 })
