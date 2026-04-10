d08bfa9035d2ce0554778744515a42948e28fba1
Your Namenicolasmaurizi <nicolasmaurizi@gmail.com>
2026-04-10 17:59:53 -0300

    feat: integrate external superhero api for image autofill

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LoadingOverlay } from './shared/components/loading-overlay/loading-overlay';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingOverlay],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
